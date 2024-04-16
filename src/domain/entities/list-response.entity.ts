4f46ce37ecd593c0fd0f36b7fb4ccd069a02e21d
DanielRicra <daniel.int@outlook.com>
2024-04-16 10:47:54 -0500

    Add ListResponseEntity

---
+++ src/domain/entities/list-response.entity.ts
@@ -0,0 +1,14 @@
+export class ListResponseEntity<T> {
+  constructor(
+    public info: {
+      count: number;
+      next: string | null;
+      previous: string | null;
+      page: number;
+      perPage: number;
+      lastPage: number;
+    },
+    public results: T[]
+  ) {}
+}
+export type ListResponseInfo = typeof ListResponseEntity.prototype.info;
