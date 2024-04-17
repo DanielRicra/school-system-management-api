812e9ea4c43ebb3b7ccfa23a73fa674507ca0e8c
DanielRicra <daniel.int@outlook.com>
2024-04-17 13:41:11 -0500

    Add validation for capacity in insertRoomSchema
    Capacity min value is 1.

--- src/db/validation-schemas.ts
+++ src/db/validation-schemas.ts
@@ -1,5 +1,8 @@
 import { createInsertSchema, createSelectSchema } from "drizzle-valibot";
 import { rooms } from "./drizzle/schema";
+import { minValue, number } from "valibot";
 
-export const insertRoomSchema = createInsertSchema(rooms);
+export const insertRoomSchema = createInsertSchema(rooms, {
+  capacity: () => number([minValue(1)]),
+});
 export const selectRoomSchema = createSelectSchema(rooms);
