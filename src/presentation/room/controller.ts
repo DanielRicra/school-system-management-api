f3d50637b82bb1c5066d8934918f162cf7535591
DanielRicra <daniel.int@outlook.com>
2024-04-17 09:53:43 -0500

    Implement createRoom method for RoomController

--- src/presentation/room/controller.ts
+++ src/presentation/room/controller.ts
@@ -1,8 +1,9 @@
 import type { RequestHandler } from "express";
 import { MainController } from "../main-controller";
 import type { RoomRepository } from "../../domain/repositories";
-import { GetRooms, GetRoom } from "../../domain/use-cases";
+import { GetRooms, GetRoom, CreateRoom } from "../../domain/use-cases";
 import { computePaginationOffsetAndLimit } from "../utils";
+import { CreateRoomDTO } from "../../domain/dtos/room";
 
 export class RoomController extends MainController {
   constructor(private readonly roomRepository: RoomRepository) {
@@ -34,7 +35,17 @@ export class RoomController extends MainController {
   };
 
   createRoom: RequestHandler = (req, res) => {
-    res.status(501).json({ message: "Not implement yet" });
+    const [errors, createRoomDTO] = CreateRoomDTO.create(req.body);
+
+    if (errors || !createRoomDTO) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+
+    new CreateRoom(this.roomRepository)
+      .execute(createRoomDTO)
+      .then((data) => res.status(201).json(data))
+      .catch((error) => this.handleErrors(error, res));
   };
 
   updateRoom: RequestHandler = (req, res) => {
