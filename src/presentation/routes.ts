4b8a512f637b37ae28b721bac178cac6a3e35f55
DanielRicra <daniel.int@outlook.com>
2024-04-17 13:44:58 -0500

    Update AppRoutes, for "/" send a list of available routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -5,6 +5,9 @@ export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
+    router.get("/", (_, res) => {
+      res.send({ routes: ["/room"] });
+    });
     router.use("/room", RoomRoutes.routes);
 
     return router;
