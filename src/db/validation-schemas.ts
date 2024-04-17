ef985db1688752cbcef356b23248cca87a3a8d7c
DanielRicra <daniel.int@outlook.com>
2024-04-17 16:55:29 -0500

    Add updateRoomSchema for room data validation
    - Introduces updateRoomSchema to validate data for updating rooms.
    - Excludes roomNumber and createdAt from update validation using `omit`

--- src/db/validation-schemas.ts
+++ src/db/validation-schemas.ts
@@ -1,8 +1,12 @@
 import { createInsertSchema, createSelectSchema } from "drizzle-valibot";
 import { rooms } from "./drizzle/schema";
-import { minValue, number } from "valibot";
+import { minValue, number, omit } from "valibot";
 
 export const insertRoomSchema = createInsertSchema(rooms, {
   capacity: () => number([minValue(1)]),
 });
+export const updateRoomSchema = omit(insertRoomSchema, [
+  "roomNumber",
+  "createdAt",
+]);
 export const selectRoomSchema = createSelectSchema(rooms);
