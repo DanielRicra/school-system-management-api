503c15af6463d1e37f88d268eddca9695c7415c6
DanielRicra <daniel.int@outlook.com>
2024-04-18 09:29:51 -0500

    Add capacity gte and lte to RoomQuery type
    - Remove & Partial Omit of Room.
    - Add optional roomNumber

--- src/domain/types.d.ts
+++ src/domain/types.d.ts
@@ -3,4 +3,7 @@ import type { Room } from "../db";
 export type RoomQuery = {
   ordering?: keyof Room;
   sortDir: "asc" | "desc";
-} & Partial<Omit<Room, "createdAt" | "updatedAt" | "id">>;
+  capacityGte?: number;
+  capacityLte?: number;
+  roomNumber?: string;
+};
