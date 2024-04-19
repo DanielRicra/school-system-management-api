2ca3deb26bead787fcdebda0bd32dce17c3ed7cf
DanielRicra <daniel.int@outlook.com>
2024-04-18 22:58:12 -0500

    Add classroom create and update dtos

---
+++ src/domain/dtos/classroom/update-classroom.dto.ts
@@ -0,0 +1,37 @@
+import { safeParse } from "valibot";
+import {
+  insertClassroomSchema,
+  updateClassroomSchema,
+} from "../../../db/validation-schemas";
+import type { DTOCreateResult } from "../../types";
+
+export class UpdateClassroomDTO {
+  private constructor(
+    public year: string,
+    public section: string,
+    public gradeLevel: "1st" | "2nd" | "3rd" | "4th" | "5th",
+    public roomId?: number | null
+  ) {}
+
+  static create(object: {
+    [key: string]: unknown;
+  }): DTOCreateResult<UpdateClassroomDTO> {
+    const result = safeParse(updateClassroomSchema, object);
+
+    if (result.success) {
+      const { gradeLevel, section, year, roomId } = result.output;
+      return [
+        undefined,
+        new UpdateClassroomDTO(year, section, gradeLevel, roomId),
+      ];
+    }
+
+    const errors: { [key: string]: string } = {};
+    for (const issue of result.issues) {
+      const key = issue.path?.[0].key as string;
+      errors[key] = issue.message;
+    }
+
+    return [errors];
+  }
+}
