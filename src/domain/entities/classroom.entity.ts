426b7dd8941b6bcf444da5c3f99bde7e6e44ef45
DanielRicra <daniel.int@outlook.com>
2024-04-18 22:44:47 -0500

    Add ClassroomEntity

---
+++ src/domain/entities/classroom.entity.ts
@@ -0,0 +1,23 @@
+export class ClassroomEntity {
+  constructor(
+    public id: number,
+    public gradeLevel: "1st" | "2nd" | "3rd" | "4th" | "5th",
+    public year: string,
+    public section: string,
+    public roomId: number | null,
+    public createdAt: Date,
+    public updatedAt: Date
+  ) {}
+
+  static getProperties() {
+    return [
+      "id",
+      "gradeLevel",
+      "year",
+      "section",
+      "roomId",
+      "createdAt",
+      "updatedAt",
+    ];
+  }
+}
