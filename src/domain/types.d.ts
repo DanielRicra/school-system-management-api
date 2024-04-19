c3789e8e011f8731d2e2eb3592d01d97fc1dd927
DanielRicra <daniel.int@outlook.com>
2024-04-18 22:53:33 -0500

    Add ClassroomQuery to domain types

--- src/domain/types.d.ts
+++ src/domain/types.d.ts
@@ -1,9 +1,18 @@
-import type { Room } from "../db";
+import type { Classroom, Room } from "../db";
+
+type SortDir = "asc" | "desc";
 
 export type RoomQuery = {
   ordering?: keyof Room;
-  sortDir: "asc" | "desc";
+  sortDir: SortDir;
   capacityGte?: number;
   capacityLte?: number;
   roomNumber?: string;
 };
+
+export type ClassroomQuery = {
+  ordering?: keyof Classroom;
+  sortDir: SortDir;
+} & Partial<Omit<Classroom, "id" | "createdAt" | "updatedAt">>;
+
+export type DTOCreateResult<T> = [(string | { [key: string]: string })?, T?];
