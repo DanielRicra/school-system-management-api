6d9b3eca6d8e7a44de4ad73477b7f72e0c8932f4
DanielRicra <daniel.int@outlook.com>
2024-04-20 00:21:04 -0500

    Add '/classroom' routes to AppRoutes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { RoomRoutes } from "./room";
+import { ClassroomRoutes } from "./classroom";
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
     router.get("/", (_, res) => {
-      res.send({ routes: ["/room"] });
+      res.send({ routes: ["/room", "/classroom"] });
     });
     router.use("/room", RoomRoutes.routes);
+    router.use("/classroom", ClassroomRoutes.routes);
 
     return router;
   }
