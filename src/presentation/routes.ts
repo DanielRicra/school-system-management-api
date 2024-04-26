58a1b6152e4bb355afe2145260eaa408c3963325
DanielRicra <daniel.int@outlook.com>
2024-04-25 21:37:24 -0500

    Add user routes to AppRouter

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import { RoomRoutes } from "./room";
 import { ClassroomRoutes } from "./classroom";
+import { UserRoutes } from "./user";
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
     router.get("/", (_, res) => {
-      res.send({ routes: ["/room", "/classroom"] });
+      res.send({ routes: ["/room", "/classroom", "/user"] });
     });
     router.use("/room", RoomRoutes.routes);
     router.use("/classroom", ClassroomRoutes.routes);
+    router.use("/user", UserRoutes.routes);
 
     return router;
   }
