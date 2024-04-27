c42e17af08edc30b1a70294415cd00f7e8574441
DanielRicra <daniel.int@outlook.com>
2024-04-26 22:46:32 -0500

    Impl update and remove methods in user controller

--- src/presentation/user/controller.ts
+++ src/presentation/user/controller.ts
@@ -1,9 +1,15 @@
 import type { RequestHandler } from "express";
 import { MainController } from "../main-controller";
 import type { UserRepository } from "../../domain/repositories";
-import { Create, FindAll, FindOne } from "../../domain/use-cases/user";
+import {
+  Create,
+  FindAll,
+  FindOne,
+  Remove,
+  Update,
+} from "../../domain/use-cases/user";
 import { computePaginationOffsetAndLimit } from "../utils";
-import { UserParamsDTO } from "../../domain/dtos/user";
+import { UpdateUserDTO, UserParamsDTO } from "../../domain/dtos/user";
 import { CreateUserDTO } from "../../domain/dtos/user";
 
 export class UserController extends MainController {
@@ -49,16 +55,43 @@ export class UserController extends MainController {
 
     new Create(this.userRepository)
       .execute(createUserDTO)
-      .then((data) => res.json(data))
+      .then((data) => res.status(201).json(data))
       .catch((err) => this.handleErrors(err, res));
   };
 
   update: RequestHandler = (req, res) => {
-    res.status(509).send("Method not implemented yet.");
+    const [paramsError, userParamsDTO] = UserParamsDTO.create(req.params.id);
+
+    if (!userParamsDTO || paramsError) {
+      res.status(400).json(paramsError);
+      return;
+    }
+
+    const [updateDTOErrors, updateUserDTO] = UpdateUserDTO.create(req.body);
+
+    if (!updateUserDTO || updateDTOErrors) {
+      res.status(400).json(updateDTOErrors);
+      return;
+    }
+
+    new Update(this.userRepository)
+      .execute(userParamsDTO.id, updateUserDTO)
+      .then((data) => res.json(data))
+      .catch((err) => this.handleErrors(err, res));
   };
 
   remove: RequestHandler = (req, res) => {
-    res.status(509).send("Method not implemented yet.");
+    const [errors, userParamsDTO] = UserParamsDTO.create(req.params.id);
+
+    if (!userParamsDTO || errors) {
+      res.status(400).json(errors);
+      return;
+    }
+
+    new Remove(this.userRepository)
+      .execute(userParamsDTO.id)
+      .then(() => res.sendStatus(204))
+      .catch((err) => this.handleErrors(err, res));
   };
 
   updatePassword: RequestHandler = (req, res) => {
