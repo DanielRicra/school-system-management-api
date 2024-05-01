f0468b6e8855ab60e3df8eda054fed047ec205be
DanielRicra <daniel.int@outlook.com>
2024-04-30 23:23:36 -0500

    Add StudentRepositoryImpl with findAll, findOne

--- src/infrastructure/repositories/index.ts
+++ src/infrastructure/repositories/index.ts
@@ -1,3 +1,4 @@
 export * from "./room.repository.impl";
 export * from "./classroom.repository.impl";
 export * from "./user.repository.impl";
+export * from "./student.repository.impl";
---
+++ src/infrastructure/repositories/student.repository.impl.ts
@@ -0,0 +1,16 @@
+import type { StudentDatasource } from "../../domain/datasources";
+import type { ListResponseEntity, StudentEntity } from "../../domain/entities";
+import type { StudentRepository } from "../../domain/repositories";
+import type { QueryParams } from "../../types";
+
+export class StudentRepositoryImpl implements StudentRepository {
+  constructor(private readonly studentDatasource: StudentDatasource) {}
+
+  findAll(query: QueryParams): Promise<ListResponseEntity<StudentEntity>> {
+    return this.studentDatasource.findAll(query);
+  }
+
+  findOne(id: StudentEntity["id"]): Promise<StudentEntity> {
+    return this.studentDatasource.findOne(id);
+  }
+}
