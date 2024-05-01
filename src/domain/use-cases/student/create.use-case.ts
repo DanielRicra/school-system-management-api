67880aba4a5732d93fc96ced57ee8591239e640b
DanielRicra <daniel.int@outlook.com>
2024-05-01 12:52:55 -0500

    Impl student's create use-case

--- src/domain/use-cases/student/create.use-case.ts
+++ src/domain/use-cases/student/create.use-case.ts
@@ -1,1 +1,10 @@
-export class Create {execute(){}}
+import type { CreateStudentDTO } from "../../dtos/student";
+import type { StudentRepository } from "../../repositories";
+
+export class Create {
+  constructor(private readonly studentRepository: StudentRepository) {}
+
+  execute(createStudentDTO: CreateStudentDTO) {
+    return this.studentRepository.create(createStudentDTO);
+  }
+}
