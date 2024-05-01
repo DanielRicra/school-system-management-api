999c74129dc60a7ce04dfdf8d9035da4bc022437
DanielRicra <daniel.int@outlook.com>
2024-04-30 23:25:05 -0500

    Impl findAll and findOne in student controller
    Add studentRepository dependency in constructor

--- src/presentation/student/controller.ts
+++ src/presentation/student/controller.ts
@@ -1,12 +1,42 @@
 import type { RequestHandler } from "express";
 import { MainController } from "../main-controller";
+import { FindAll, FindOne } from "../../domain/use-cases/student";
+import type { StudentRepository } from "../../domain/repositories";
+import { computePaginationOffsetAndLimit } from "../utils";
+import { isUUIDFormat } from "../../domain/dtos/utils";
+
 export class StudentController extends MainController {
+  constructor(private readonly studentRepository: StudentRepository) {
+    super();
+  }
+
   findAll: RequestHandler = (req, res) => {
-    res.status(501).json({ error: "Not implemented yet." });
+    const { page, per_page: perPage, ...query } = req.query;
+    const { limit, offset } = computePaginationOffsetAndLimit(page, perPage);
+
+    new FindAll(this.studentRepository)
+      .execute({
+        limit,
+        offset,
+        otherParams: query as { [key: string]: string },
+      })
+      .then((data) => res.json(data))
+      .catch((err) => this.handleErrors(err, res));
   };
 
   findOne: RequestHandler = (req, res) => {
-    res.status(501).json({ error: "Not implemented yet." });
+    const studentId = isUUIDFormat(req.params.id) ? req.params.id : undefined;
+
+    if (!studentId) {
+      res.status(400).json({
+        message: "The id(UUID) is badly formatted.",
+      });
+      return;
+    }
+    new FindOne(this.studentRepository)
+      .execute(studentId)
+      .then((data) => res.json(data))
+      .catch((err) => this.handleErrors(err, res));
   };
 
   create: RequestHandler = (req, res) => {
