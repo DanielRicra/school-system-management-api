a8313e635e1a3b6c03a78858143dcf2c7305038f
DanielRicra <daniel.int@outlook.com>
2024-05-01 17:16:45 -0500

    Improve error message formatting in API responses
    - Updates error responses to use message property instead of error.
    - Provides a clearer structure for error messages.

--- src/presentation/main-controller.ts
+++ src/presentation/main-controller.ts
@@ -4,8 +4,8 @@ import { CustomError } from "../domain/errors";
 export class MainController {
   protected handleErrors = (error: unknown, res: Response) => {
     if (error instanceof CustomError) {
-      return res.status(error.statusCode).json({ error: error.message });
+      return res.status(error.statusCode).json({ message: error.message });
     }
-    return res.status(500).json({ error: "Internal server error" });
+    return res.status(500).json({ message: "Internal server error" });
   };
 }
