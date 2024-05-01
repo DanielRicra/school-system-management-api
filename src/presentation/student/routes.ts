9c6be09236b04adce3e66cf038f0e9baf961aaa1
DanielRicra <daniel.int@outlook.com>
2024-04-30 23:26:41 -0500

    Inject instances of dtsrc and repo. student routes

--- src/presentation/student/routes.ts
+++ src/presentation/student/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { StudentController } from "./controller";
+import { StudentDatasourceImpl } from "../../infrastructure/datasources";
+import { StudentRepositoryImpl } from "../../infrastructure/repositories";
 export class StudentRoutes {
   static get routes(): Router {
     const router = Router();
 
-    const controller = new StudentController();
+    const datasource = new StudentDatasourceImpl();
+    const repository = new StudentRepositoryImpl(datasource);
+    const controller = new StudentController(repository);
 
     router.route("/").get(controller.findAll).post(controller.create);
     router
