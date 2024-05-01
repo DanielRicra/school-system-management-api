8454ab41a46a2f156b02dbdabbf3630c8c86a4d7
DanielRicra <daniel.int@outlook.com>
2024-05-01 17:05:24 -0500

    Add student's Patch use-case

--- src/domain/use-cases/student/patch.use-case.ts
+++ src/domain/use-cases/student/patch.use-case.ts
@@ -1,1 +1,10 @@
-export class Patch {execute(){}}
+import type { PatchStudentDTO } from "../../dtos/student";
+import type { StudentRepository } from "../../repositories";
+
+export class Patch {
+  constructor(private readonly studentRepository: StudentRepository) {}
+
+  execute(id: string, patchStudentDTO: PatchStudentDTO) {
+    return this.studentRepository.patch(id, patchStudentDTO);
+  }
+}
