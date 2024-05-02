5e40226033f1c59da1997633209761230308609a
DanielRicra <daniel.int@outlook.com>
2024-05-01 20:04:18 -0500

    Add student Remove use-case
    - To be use in controller to handle the delete of the student.

--- src/domain/use-cases/student/remove.use-case.ts
+++ src/domain/use-cases/student/remove.use-case.ts
@@ -1,1 +1,9 @@
-export class Remove {execute(){}}
+import type { StudentEntity } from "../../entities";
+import type { StudentRepository } from "../../repositories";
+
+export class Remove {
+  constructor(private readonly studentRepository: StudentRepository) {}
+  execute(id: StudentEntity["id"]): Promise<void> {
+    return this.studentRepository.remove(id);
+  }
+}
