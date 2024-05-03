ffdaac5990ee2c98f0894e3ee7ee30a3432c6968
DanielRicra <daniel.int@outlook.com>
2024-05-02 21:51:43 -0500

    Use teacher routes in AppRoutes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -3,18 +3,22 @@ import { RoomRoutes } from "./room";
 import { ClassroomRoutes } from "./classroom";
 import { UserRoutes } from "./user";
 import { StudentRoutes } from "./student";
+import { TeacherRoutes } from "./teacher";
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
     router.get("/", (_, res) => {
-      res.send({ routes: ["/room", "/classroom", "/user", "/student"] });
+      res.send({
+        routes: ["/room", "/classroom", "/user", "/student", "/teacher"],
+      });
     });
     router.use("/room", RoomRoutes.routes);
     router.use("/classroom", ClassroomRoutes.routes);
     router.use("/user", UserRoutes.routes);
     router.use("/student", StudentRoutes.routes);
+    router.use("/teacher", TeacherRoutes.routes);
 
     return router;
   }
