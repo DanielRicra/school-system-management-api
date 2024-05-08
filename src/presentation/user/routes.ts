6f91fa2d3e4622b383479b0b2a3a4ba516fbdf55
DanielRicra <daniel.int@outlook.com>
2024-05-08 18:59:47 -0500

    Add middleware to check for admin in user routes
    The routes GET"/" , POST"/", and DELETE "/id", has checkUserRole("admin") middleware

--- src/presentation/user/routes.ts
+++ src/presentation/user/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { UserController } from "./controller";
 import { UserDatasourceImpl } from "../../infrastructure/datasources";
 import { UserRepositoryImpl } from "../../infrastructure/repositories";
+import { AuthMiddleware } from "../middlewares";
 
 export class UserRoutes {
   static get routes(): Router {
@@ -11,12 +12,16 @@ export class UserRoutes {
     const repository = new UserRepositoryImpl(datasource);
     const controller = new UserController(repository);
 
-    router.get("/", controller.findAll);
+    router.get("/", AuthMiddleware.checkUserRole("admin"), controller.findAll);
     router.get("/:id", controller.findOne);
-    router.post("/", controller.create);
+    router.post("/", AuthMiddleware.checkUserRole("admin"), controller.create);
     router.put("/:id", controller.update);
     router.patch("/:id", controller.patch);
-    router.delete("/:id", controller.remove);
+    router.delete(
+      "/:id",
+      AuthMiddleware.checkUserRole("admin"),
+      controller.remove
+    );
     router.put("/:id/password", controller.updatePassword);
 
     return router;
