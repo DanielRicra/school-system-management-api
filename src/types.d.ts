56656c4b9a755dbfdf8e2bd6692bd81c9627608d
DanielRicra <daniel.int@outlook.com>
2024-05-08 18:52:53 -0500

    Add more more types, in src/types
    - Add AuthTokenPayload type
    - declare a global namespace for Express Locals type to be user? of type User of schema.

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,22 @@
+import type { User } from "./db";
+
 export type QueryParams = {
   limit: number;
   offset: number;
   otherParams: { [key: string]: string };
 };
+
+export type AuthTokenPayload = {
+  userId: string;
+  code: string;
+  firstName: string;
+  surname: string;
+};
+
+declare global {
+  namespace Express {
+    interface Locals {
+      user?: { role: User["role"] } & AuthTokenPayload;
+    }
+  }
+}
